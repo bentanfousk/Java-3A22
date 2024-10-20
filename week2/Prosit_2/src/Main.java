34d87b9cdd0e80f630528c5d82f17964de798e15
khalil <Mohamedkhalil.Tanfous@esprit.tn>
2024-10-20 17:07:50 +0100

    prosit3

--- week2/Prosit_2/src/Main.java
+++ week2/Prosit_2/src/Main.java
@@ -4,9 +4,27 @@ public class Main {
 
     public static void main(String[] args) {
         Animal lion=new Animal("cats","lion",20,true);
+        Animal bob=new Animal("cats","bob",20,true);
+        Animal me3za=new Animal("cats","me3za",20,true);
         ZooManagment zoo=new ZooManagment("myZoo","Tunis");
+
+
+        zoo.showAnimals();
+        if(zoo.addAnimal(lion)) System.out.println("lion added");
+        if(zoo.addAnimal(bob)) System.out.println("bob added");
+        if(zoo.addAnimal(me3za)) {System.out.println("me3za added");}
+        else System.out.println("me3za not added");
+
+
+
         System.out.println(zoo.toString());
-        System.out.println(lion.toString());
+        zoo.showAnimals();
+//        System.out.println(zoo.searchAnimal(lion));
+//        System.out.println(zoo.searchAnimal(bob));
+//        System.out.println(zoo.searchAnimal(me3za));
+
+
+
 
     }
 }
--- week2/Prosit_2/src/ZooManagment.java
+++ week2/Prosit_2/src/ZooManagment.java
@@ -2,10 +2,11 @@
 
 public class ZooManagment {
 
-    Animal [] animals;
-    int NBRCAGES=25;
+    Animal[] animals;
+    static final int NBRCAGES=25;
     String Name ;
     String city;
+    int nbAnimeaux=0;
 
     ZooManagment(String Name, String city) {
 
@@ -14,13 +15,78 @@ public class ZooManagment {
         animals = new Animal[NBRCAGES];
 
     }
+
+    @Override
     public String toString() {
+
         return "name:" + Name + " |city:" + city ;
     }
 
+   public boolean addAnimal(Animal a) {
+        if(searchAnimal(a)!=-1)
+            return false;
 
-//    void displayInformation(){
-//        System.out.println(zooName + " has " + nbrCages + " cages");
-//    }
+
+        if (nbAnimeaux < NBRCAGES) {
+            animals[nbAnimeaux++] = a;
+            return true;
+
+        }
+        return false;
+
+   }
+   public void showAnimals() {
+        if(nbAnimeaux<1){
+            System.out.println("No animals found");
+        }
+        else {
+            for(int i=0;i<nbAnimeaux;i++){
+
+                System.out.println("Animal "+ (i+1) +": "+animals[i].toString());
+            }
+        }
+   }
+
+    public int searchAnimal(Animal animal){
+        int index=0;
+        while(index<nbAnimeaux && !animals[index].equals(animal)){
+            index++;
+        }
+
+        if(index==nbAnimeaux){
+            return -1;
+        }
+
+        return index;
+
+    }
+
+    public boolean removeAnimal(Animal animal){
+        int index=searchAnimal(animal);
+        if(index!=-1){
+            for(int i=index+1;i<nbAnimeaux;i++){
+                animals[i]=animals[i+1];
+            }
+            nbAnimeaux--;
+            animals[index]=null;
+            return true;
+        }
+        return false;
+
+    }
+
+    public boolean isZooFull(){
+        return nbAnimeaux == NBRCAGES;
+    }
+
+
+    public ZooManagment comparerZoo(ZooManagment z1, ZooManagment z2){
+        if(z1.nbAnimeaux> z2.nbAnimeaux){
+            return z1;
+        }
+        else
+            return z2;
+    }
+
 
 }
